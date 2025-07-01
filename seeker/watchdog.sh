04b674e4191fae5506010497b6f5ccc16cd4bc14
James Unger <james.unger@powerschool.com>
2025-07-01 07:48:32 -0400

    Kill ipfs if using too much memory.

--- seeker/watchdog.sh
+++ seeker/watchdog.sh
@@ -5,7 +5,7 @@ while true; do
         RSS=$(ps -p `pidof ipfs` -o rss | tail -n 1)
         echo "$RSS"
         if (( $RSS >= 2000000 )); then
-                echo kill `pidof ipfs`
+                kill `pidof ipfs`
         fi
 	sleep 5
 done
